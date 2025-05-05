e3342ef11cf1bf2216a084b45962979a42ae03b1
Peubacovis <peubacovis@gmail.com>
2025-05-05 00:43:11 -0300

    fix:

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,7 @@ import { LoginDto } from './dto/login.dto';  // Crie um DTO para login
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
